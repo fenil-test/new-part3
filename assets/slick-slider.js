45068f292d2d0e9783306265b5815e3cc2b97e80
fenil-test <169125616+fenil-test@users.noreply.github.com>
2024-06-03 18:19:38 +0530

    first commit

---
+++ assets/slick-slider.js
@@ -0,0 +1,32 @@
+$('.my-slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1150,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+ 
+    ]
+  });
